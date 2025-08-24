0b477487b3f7cb4adc2cee3ec93d258aec3eab71
yeeunsong1002 <songyeeun1002@gmail.com>
2025-08-24 10:27:56 +0900

    test

---
+++ src/main/java/com/koreait/exam/App.java
@@ -0,0 +1,110 @@
+package com.koreait.exam;
+
+import java.util.Collections;
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+
+
+
+public class App {
+
+    private Scanner sc;
+
+    public App(Scanner sc) {
+        this.sc = sc;
+    }
+
+
+    public void run() {
+        System.out.println("== 명언 앱 ==");
+        int lastId = 0;
+
+        Motivation m1 = null;
+        Motivation m2 = null;
+        Motivation m3 = null;
+
+        List<Motivation> motivationList = new ArrayList<>();
+
+
+
+
+
+        while (true) {
+
+            System.out.print("명령어) ");
+            String cmd = sc.nextLine().trim();
+
+
+
+
+//            if (lastId == 1) {
+//                m1 = addMotiv;
+//            }
+//
+//            if (lastId == 2) {
+//                m2 = addMotiv;
+//            }
+
+            if (cmd.equals("exit")) {
+                System.out.println("명언 앱 종료");
+                break;
+            } else if (cmd.equals("add")) {
+                System.out.print("명언 : ");
+                String body = sc.nextLine().trim();
+                System.out.print("저자 : ");
+                String author = sc.nextLine().trim();
+
+                lastId++;
+
+                Motivation addMotiv = new Motivation(lastId, body, author);
+                motivationList.add(addMotiv);
+
+                System.out.println(lastId + "번 명언이 등록되었습니다.");
+
+//                m1 = new Motivation();
+//                m2 = new Motivation();
+//                m1.setId(lastId);
+//                m1.setBody(body);
+//                m1.setAuthor(author);
+//                m2.setId(lastId);
+//                m2.setBody(body);
+//                m2.setAuthor(author);
+
+
+
+
+            } else if (cmd.equals("list")) {
+                System.out.println("=".repeat(20));
+                System.out.println("   번호      /       저자       /       명언");
+
+                if(motivationList.size() == 0) {
+                    System.out.println("등록된 명언이 없습니다.");
+                }
+                else {
+
+                    Collections.reverse(motivationList);
+                    for (Motivation m : motivationList) {
+                        System.out.println(m.getId() + "     " + m.getBody() + "     " + m.getAuthor());
+
+                    }
+
+                /*    for(int i = motivationList.size() - 1; i >= 0 ; i--){
+                        System.out.println(motivationList.get(i).getId() + "     " + motivationList.get(i).getBody() + "     " + motivationList.get(i).getAuthor() );
+                    }
+                }*/
+
+
+//                System.out.println(m1.getAuthor());
+//                System.out.println(m1.getBody());
+//                System.out.println(m1.getId());
+                }
+            } else {
+                System.out.println("사용할 수 없는 명령어입니다.");
+            }
+
+        }
+    }
+
+
+}
--- src/main/java/com/koreait/exam/Main.java
+++ src/main/java/com/koreait/exam/Main.java
@@ -1,12 +1,17 @@
 package com.koreait.exam;
+
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("== 명언 앱 ==");
 
+        Scanner sc = new Scanner(System.in);
 
-        }
+        new App(sc).run();
+
+        sc.close();
+
     }
+}
